29223a62f698f63e04d674af9a361784a2549a7a
ATSxp <allantavares456@gmail.com>
2022-06-17 09:36:34 -0300

    rom created

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
+cls
 mkdir build
 mkdir rom
-cls
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o build/main.o source/main.c
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o rom/game.gb build/main.o
 cd rom
---
+++ source/main.c
@@ -0,0 +1,11 @@
+#include <gb/gb.h>
+#include <stdio.h>
+
+int main(){
+    printf("Ola mundo");
+
+    while(1){
+        
+    }
+    
+}
